944800efd2e84e847c9333cceeb7bd7ca4b16ef1
thelon <jps_90@gmx.de>
2026-05-05 13:54:13 +0200

    fix: replace @DynamicPropertySource with @ServiceConnection

--- backend/analytics/src/test/java/com/energy/analytics/BaseIntegrationTest.java
+++ backend/analytics/src/test/java/com/energy/analytics/BaseIntegrationTest.java
@@ -2,10 +2,10 @@ package com.energy.analytics;
 
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
@@ -15,21 +15,12 @@ import org.testcontainers.utility.DockerImageName;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public abstract class BaseIntegrationTest {
 
+   @Container
+   @ServiceConnection
    static PostgreSQLContainer<?> timescale = new PostgreSQLContainer<>(
            DockerImageName.parse("timescale/timescaledb:latest-pg16")
                    .asCompatibleSubstituteFor("postgres")
    );
-
-   static {
-      timescale.start();
-   }
-
-   @DynamicPropertySource
-   static void configureProperties(DynamicPropertyRegistry registry) {
-      registry.add("spring.datasource.url", timescale::getJdbcUrl);
-      registry.add("spring.datasource.username", timescale::getUsername);
-      registry.add("spring.datasource.password", timescale::getPassword);
-   }
 
 }
 
