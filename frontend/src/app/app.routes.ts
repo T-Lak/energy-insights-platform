c498f4fb8271843c4c955054e04b3522a0e20466
thelon <jps_90@gmx.de>
2026-05-20 11:53:57 +0200

    feat: implement routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Dashboard } from './features/dashboard/dashboard';
+import { RenewableShare } from './features/renewable-share/renewable-share';
+import { CrossborderFlows } from './features/crossborder-flows/crossborder-flows';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: Dashboard,
+  },
+  {
+    path: 'analytics',
+    children: [
+      {
+        path: 'renewable-share',
+        component: RenewableShare,
+      },
+      {
+        path: 'crossborder-flows',
+        component: CrossborderFlows,
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
