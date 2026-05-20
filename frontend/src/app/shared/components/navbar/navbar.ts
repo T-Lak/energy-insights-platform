91afc4e9148f98862717bacc88d3d8d9dc4da93e
thelon <jps_90@gmx.de>
2026-05-20 12:02:20 +0200

    add styling for active routes

--- frontend/src/app/shared/components/navbar/navbar.ts
+++ frontend/src/app/shared/components/navbar/navbar.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
