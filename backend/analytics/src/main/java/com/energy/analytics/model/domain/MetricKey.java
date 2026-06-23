33f97eaeb1e6d074412cbbf8ba38dda45d0e0624
thelon <jps_90@gmx.de>
2026-06-23 10:49:37 +0200

    feat(top sources): introduce enums for maintainability.

---
+++ backend/analytics/src/main/java/com/energy/analytics/model/domain/MetricKey.java
@@ -0,0 +1,18 @@
+package com.energy.analytics.model.domain;
+
+import lombok.Getter;
+
+@Getter
+public enum MetricKey {
+
+   RENEWABLE_SHARE("renewable_share"),
+   CARBON_INTENSITY("carbon_intensity"),
+   TOTAL_LOAD("total_load"),
+   NET_BALANCE("net_balance");
+
+   private final String key;
+
+   MetricKey(String key) {
+      this.key = key;
+   }
+}
