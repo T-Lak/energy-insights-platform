bee485f491de1d359c99c6458d6ec43b5f59c82a
thelon <jps_90@gmx.de>
2026-07-07 15:12:23 +0200

    chore(cleanup): remove unnecessary comments.

--- frontend/src/app/features/dashboard/widgets/widgets.ts
+++ frontend/src/app/features/dashboard/widgets/widgets.ts
@@ -3,10 +3,9 @@ import { Component, OnInit } from '@angular/core';
 import { Widget } from './widget/widget';
 
 import { BarChart } from '../../../shared/components/bar-chart/bar-chart';
-import { BarChartItem } from '../../../shared/components/bar-chart/bar-chart.model';
 import { CommonModule } from '@angular/common';
 import { SourceRankingPointDTO } from '../../../core/model/dto/source-ranking-point.dto';
-import { Observable, switchMap, map } from 'rxjs';
+import { Observable } from 'rxjs';
 import { WidgetsService } from './widgets.service';
 import { TopSourcesCategory } from '../dashboard.model';
 
@@ -14,7 +13,7 @@ import { TopSourcesCategory } from '../dashboard.model';
   selector: 'app-widgets',
   standalone: true,
   imports: [CommonModule, Widget, BarChart],
-  providers: [WidgetsService], // Re-created on component init, which is fine if shared!
+  providers: [WidgetsService],
   templateUrl: './widgets.html',
   styleUrl: './widgets.scss',
 })
@@ -27,7 +26,6 @@ export class Widgets implements OnInit {
   constructor(private widgetsService: WidgetsService) {}
 
   ngOnInit(): void {
-    // Single HTTP pipeline invocation point!
     this.dictionaryStream$ = this.widgetsService.getWidgetsData();
   }
 }
